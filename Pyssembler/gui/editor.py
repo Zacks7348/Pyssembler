f247ecfaea684c11e94fdc4abac9df126080c2ae
Zacks7348 <zack@zschreiner.dev>
2021-11-26 22:58:15 -0500

    click file in explorer opens an editor for the file

--- Pyssembler/gui/editor.py
+++ Pyssembler/gui/editor.py
@@ -8,6 +8,9 @@ import os
 
 from .cmd import CommandLine
 
+from Pyssembler.mips.instructions import get_mnemonics
+import Pyssembler.mips.hardware.registers as regs
+
 
 class IDEPage(tk.Frame):
     """
@@ -21,7 +24,7 @@ class IDEPage(tk.Frame):
         self.__init_ui()
 
     def __init_ui(self):
-        self.explorer = Explorer(self, self.manager, show='tree')
+        self.explorer = Explorer(self, self.manager, show='tree', selectmode=tk.BROWSE)
         self.exp_ybar = tk.Scrollbar(
             self, orient=tk.VERTICAL, command=self.explorer.yview)
         self.exp_xbar = tk.Scrollbar(
@@ -40,8 +43,9 @@ class IDEPage(tk.Frame):
         self.editor.place(relwidth=0.9, relheight=1, relx=0.1)
 
     def on_explorer_select(self, event):
-        print(self.explorer.selection())
-
+        path = self.explorer.paths.get(self.explorer.selection()[0], None)
+        if path:
+            self.editor.open_editor(path)
 
 class Explorer(ttk.Treeview):
     """
@@ -51,9 +55,10 @@ class Explorer(ttk.Treeview):
     def __init__(self, master, manager, **kwargs):
         super().__init__(master, **kwargs)
         self.manager = manager
+        self.paths = {} # Used to save abs paths while displaying filenames
         self.heading('#0', text='Explorer', anchor='w')
         # FOR TESTING
-        self.add_path('Pyssembler/work')
+        self.add_path(os.path.abspath('Pyssembler/work'))
 
     def add_path(self, path):
         """
@@ -69,6 +74,9 @@ class Explorer(ttk.Treeview):
             old = self.insert(parent, tk.END, text=p, open=False)
             if os.path.isdir(abspath):
                 self.__add_paths(old, abspath)
+            else:
+                # Is file
+                self.paths[old] = abspath
 
 
 class EditorManager(ttk.Notebook):
@@ -104,31 +112,28 @@ class EditorManager(ttk.Notebook):
     def close_editor(self, editor):
         if editor.saved:
             self.forget(editor)
-            return
+            return True
         s = messagebox.askyesnocancel(
             title='Save', message=f'Do you want to save {os.path.basename(editor.path)}?')
-        if s: 
-            editor.save()
-            self.forget(editor)
-            return
-        if s == False:
-            self.forget(editor)
         if s is None:
             return False
         if s:
             editor.save()
         self.forget(editor)
-        self.open_editors.pop(editor.path)
         return True
     
     def close_editors(self):
         """
         Close all open editors
         """
+        removed = []
         for editor in self.open_editors.values():
             if not self.close_editor(editor):
+                # User clicked Cancel, don't exit
                 return False
-            self.open_editors.pop(editor.path)
+            removed.append(editor.path)
+        for p in removed:
+            self.open_editors.pop(p)
         return True
         
 
@@ -153,7 +158,9 @@ class EditorManager(ttk.Notebook):
 
         index = self.index("@%d,%d" % (event.x, event.y))
         if self._active == index:
-            self.close_editor(self.nametowidget(self.tabs()[index]))
+            editor = self.nametowidget(self.tabs()[index])
+            self.close_editor(editor)
+            self.open_editors.pop(editor.path)
             self.event_generate("<<NotebookTabClosed>>")
 
         self.state(["!pressed"])
@@ -236,8 +243,9 @@ class Editor(tk.Frame):
         self.text.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
         self.vsb.pack(side=tk.LEFT, fil=tk.Y)
 
-        self.text.bind("<<Change>>", self._on_change)
-        self.text.bind("<Configure>", self._on_change)
+        self.text.bind('<<Change>>', self._on_change)
+        self.text.bind('<KeyRelease>', self.on_key_release)
+        self.text.bind('<Configure>', self._on_change)
 
     def save(self):
         """
@@ -249,11 +257,19 @@ class Editor(tk.Frame):
 
     def _on_change(self, event=None):
         self.linenums.redraw()
+    
+    def on_key_release(self, event=None):
+        """
+        When the user releases a key, perform syntax highlighting
+        on that line and set saved status to False
+        """
+        self.text.highlight_syntax_line()
         self.saved = False
 
     def __read_file(self):
         with open(self.path) as f:
             self.text.insert(tk.INSERT, f.read())
+        self.text.highlight_syntax()
         self.saved = True
 
 
@@ -287,10 +303,103 @@ class EditorText(tk.Text):
         self.tk.call("rename", self._w, self._orig)
         self.tk.createcommand(self._w, self._proxy)
 
+        # Set up syntax highlighting tags
+        self.tag_config('instr', foreground='dark orange')
+        self.tag_config('reg', foreground='dodger blue')
+        self.tag_config('comment', foreground='gray47')
+        self.tag_config('error', foreground='red')
+
+        # Save regex expressions for syntax highlighting
+        self.mnemonic_regex = '|'.join(get_mnemonics())
+        self.reg_regex = '|'.join([r.replace("$", "\$") for r in regs.get_names()])
+
+    def remove_highlight_syntax(self):
+        start = "1.0"
+        end = "end"
+        self.tag_remove("comment", start, end)
+        self.tag_remove("instr", start, end)
+        self.tag_remove("reg", start, end)
+
+    def highlight_syntax(self):
+        start = '1.0'
+        end = self.index('end')
+        self.mark_set("matchStart", start)
+        self.mark_set("matchEnd", start)
+        self.mark_set("searchLimit", end)
+        for i in range(1, int(float(end))+1):
+            searchStart = self.index('{}.0'.format(i))
+            searchEnd = self.index('{} lineend'.format(searchStart))
+            self.highlight_syntax_line(searchStart, searchEnd)
+    
+    def highlight_syntax_line(self, start=None, stop=None):
+        if start is None:
+            self.mark_set("searchStart", self.index("insert linestart"))
+        else:
+            self.mark_set('searchStart', start)
+        if stop is None:
+            self.mark_set("searchEnd", self.index("insert lineend"))
+        else:
+            self.mark_set('searchEnd', stop)
+
+        # If comment exists, mark everything right of # as comment
+        index = self.search('#', 'searchStart', 'searchEnd', regexp=True)
+        if index == '':
+            self.tag_remove('comment', 'searchStart', 'searchEnd')
+        else:
+            self.mark_set('matchStart', index)
+            self.tag_add('comment', 'matchStart', 'searchEnd')
+            self.mark_set('searchEnd', self.index('matchStart'))
+        
+        # Search for and highlight instruction mnemonics
+        # There should only be a max of 1 mnemonic per line,
+        # so only highlight first found
+
+        # TODO: Mnemonics surrounded by other chars are getting colored
+        # ie .word, or would be found and colored. Need to find solution
+        count = tk.IntVar()
+        index = self.search(
+            self.mnemonic_regex, "searchStart", "searchEnd", count=count, regexp=True
+        )
+        if index == "":
+            self.tag_remove("instr", "searchStart", "searchEnd")
+        else:
+            self.mark_set("matchStart", index)
+            self.mark_set("matchEnd", "{}+{}c".format(index, count.get()))
+            self.tag_add("instr", "matchStart", "matchEnd")
+        
+        # Search for and highlight all reg names
+        # TODO: Register names surrounded by other chars are getting colored
+        # ie abcd$s1abc, $s1 would be found and colored. Need to find solution
+        self.tag_remove("reg", "searchStart", "searchEnd")
+        while True:
+            index = self.search(
+                self.reg_regex,
+                "searchStart",
+                "searchEnd",
+                count=count,
+                regexp=True,
+            )
+            if index == "":
+                self.tag_remove("reg", "searchStart", "searchEnd")
+                break
+            else:
+                self.mark_set("matchStart", index)
+                self.mark_set("matchEnd", "{}+{}c".format(index, count.get()))
+                self.mark_set("searchStart", "matchEnd+1c")
+                self.tag_add("reg", "matchStart", "matchEnd")
+
     def _proxy(self, *args):
         # let the actual widget perform the requested action
         cmd = (self._orig,) + args
-        result = self.tk.call(cmd)
+        try:
+            # BAND-AID FIX!!!!!!!!!!!!!!!1
+            # Copy-Pasting crashes program, most likely due to
+            # those events firing first then the selection being
+            # removed. Other events could also crash
+            # For now just catch and move on with my life
+            result = self.tk.call(cmd)
+        except:
+            return None
 
         # generate an event if something was added or deleted,
         # or the cursor position changed
@@ -305,3 +414,4 @@ class EditorText(tk.Text):
 
         # return what the actual widget returned
         return result
+
