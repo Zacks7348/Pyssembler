47e0c2d8a416cd04717ef12b493974ace4e70bda
Zackary Schreiner <zack@zschreiner.dev>
2024-01-17 23:59:25 -0500

    Removed deprecated code

--- pyssembler/utils.py
+++ pyssembler/utils.py
@@ -1,5 +1,4 @@
 import logging
-from pathlib import Path
 
 
 class LoggableMixin:
@@ -9,19 +8,3 @@ class LoggableMixin:
         if suffix is not None:
             name += f'.{suffix}'
         return logging.getLogger(f'{self.__class__.__module__}.{name}')
-
-
-class Location:
-    def __init__(
-            self,
-            path: Path = None,
-            line: int = None,
-            line_char: int = None,
-            char: int = None):
-        self.path: Path = path
-        self.line: int = line
-        self.line_char: int = line_char
-        self.char: int = char
-
-    def __str__(self):
-        return f'{self.path}({self.line},{self.char})'
